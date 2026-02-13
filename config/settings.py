23c3f509aaffbeb37d652ef9678504d417da8470
pitweazle <rechen@trainer.schule>
2026-02-13 22:38:22 +0100

    alle hosts erlaubt

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Erlaubte Hosts als Liste (in .env mit Komma getrennt)
-ALLOWED_HOSTS = ['physiktrainer.app', 'www.physiktrainer.app', 'rt.uber.space', '127.0.0.1', 'localhost']
+#ALLOWED_HOSTS = ['physiktrainer.app', 'www.physiktrainer.app', 'rt.uber.space', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
+
 
 # 4. Weiche für Uberspace-Erkennung
 ON_UBERSPACE = 'caelum' in socket.gethostname()
